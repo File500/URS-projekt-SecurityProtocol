5cac44836ddef1f1fbea73ac609cf0f4130a3a1a
File500 <96439801+File500@users.noreply.github.com>
2022-02-19 17:19:45 +0100

    Add files via upload

--- Projekt/kod_za_sound.c
+++ Projekt/kod_za_sound.c
@@ -1,26 +1,40 @@
 #define F_CPU 7372800UL
 #include <avr/io.h>
 #include <util/delay.h>
-#define LED_OUTPUT 3
-#define PIR_INPUT  0
-#define SOUND_INPUT 1
+#define LED_OUTPUT_SOUND 3
+#define LED_OUTPUT_PIR 0
+#define PIR_INPUT  7
+#define SOUND_INPUT 2
 
 int main(void)
 {
-	DDRC = 0x00;
-	DDRA = 0x00;	/* Set the PIR port as input port */
+	DDRC = 0x00; 
 	DDRB = 0xff;
-	DDRD = 0xff;	/* Set the LED port as output port */
+	DDRA = 0xff;
+	DDRD = 0x00;
+	
+	PORTA = 0xff;
+
 
 	while(1)
 	{
 		
 		 if(PINC & _BV(SOUND_INPUT)){
-			PORTB = _BV(LED_OUTPUT);
-			_delay_ms(2000);
-			PORTB = 0x00;
+			PORTB |= _BV(LED_OUTPUT_SOUND);
+			PORTA = 0b11101111;
 			_delay_ms(2000);
 		} 
-	
+		
+		
+		if(PIND & _BV(PIR_INPUT)){
+			PORTB |= _BV(LED_OUTPUT_PIR);
+			//while((PINB & _BV(PIR_INPUT)));
+			_delay_ms(10000);
+		}
+		
+		
+		PORTB = 0x00;
+		
+		_delay_ms(2000);
 	}
 }
