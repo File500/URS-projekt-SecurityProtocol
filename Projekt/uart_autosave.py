bb68ebff5aba73be6474ed8d11e342f9ee32a1af
DaveXo9 <85836822+DaveXo9@users.noreply.github.com>
2022-02-27 12:26:30 +0100

    Update uart_autosave.py

--- Projekt/uart_autosave.py
+++ Projekt/uart_autosave.py
@@ -3,7 +3,6 @@ import serial
 # Port that is used for UART communication
 serialPort = serial.Serial(port="COM3", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 serialString = ""
-f = open("alarm_records.txt", "a")
 
 while 1:
         # Wait until there is data waiting in the serial buffer
@@ -16,7 +15,7 @@ while 1:
             contents: str = serialString.decode('utf-8').rstrip()
 
             print(contents)
-            f = open("alarm_records.txt", "a")    
+            f = open("alarm_records.txt", "a+")    
             f.write(contents)
             f.write('\n')    
             f.close()
